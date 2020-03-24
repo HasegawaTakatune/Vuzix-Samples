a64ad3e665f28d9b8415f33f5e473c6d9134b19f
Takatune <0ta5274738t4p4r@gmail.com>
2020-03-24 23:47:02 +0900

    バーコードリーダー処理のバグ取り

--- BarcodeReader/app/src/main/java/com/example/barcodereader/MainActivity.java
+++ BarcodeReader/app/src/main/java/com/example/barcodereader/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -25,8 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
-            //Log.d("readQR", result.getContents());
-
             Intent intent = new Intent(getApplicationContext(), ShowResult.class);
             intent.putExtra(INPUT_RESULT, result.getContents());
             startActivity(intent);
--- BarcodeReader/app/src/main/java/com/example/barcodereader/ShowResult.java
+++ BarcodeReader/app/src/main/java/com/example/barcodereader/ShowResult.java
@@ -2,41 +2,41 @@ package com.example.barcodereader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.json.JSONArray;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ShowResult extends AppCompatActivity {
 
-    private static final String SCAN_DATA = "SCAN_DATA";
-    private static final String KEY_TOTAL = "KEY_TOTAL";
-    private static final String KEY_MASTAR = "KEY_MASTAR";
+    public static final String SCAN_DATA = "SCAN_DATA";
+    public static final String KEY_TOTAL = "KEY_TOTAL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        TextView nameView = findViewById(R.id.name);
-        TextView priceView = findViewById(R.id.price);
-        TextView totalView = findViewById(R.id.total);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_result);
 
+        TextView nameView = findViewById(R.id.name);
+        TextView priceView = findViewById(R.id.price);
+        TextView totalView = findViewById(R.id.total);
+
         String scanText = getIntent().getStringExtra(MainActivity.INPUT_RESULT);
-        String[] split = scanText.split(":");
+        String[] split = scanText.split("：");
 
         if (split.length < 0 || 2 < split.length || !isNumber(split[1])) return;
 
         nameView.setText("品名 : " + split[0]);
-        priceView.setText(" 値段 : " + split[1] + "\\");
+        priceView.setText(" 値段 : " + split[1] + "円");
 
         SharedPreferences sharedPreferences = getSharedPreferences(SCAN_DATA, MODE_PRIVATE);
         int total = sharedPreferences.getInt(KEY_TOTAL, 0);
-        String strJson = sharedPreferences.getString(KEY_MASTAR,"");
 
         total += Integer.parseInt(split[1]);
         totalView.setText("合計金額 : " + total);
@@ -45,10 +45,15 @@ public class ShowResult extends AppCompatActivity {
         editor.putInt(KEY_TOTAL, total);
         editor.apply();
 
-        ArrayList<String> mastorList = LoadList();
-        mastorList.add(scanText);
-        SaveList(mastorList);
-
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        };
+        timer.schedule(task, 5 * 1000);
     }
 
     public boolean isNumber(String num) {
@@ -60,27 +65,31 @@ public class ShowResult extends AppCompatActivity {
         }
     }
 
-    private void SaveList(ArrayList<String> list){
-        JSONArray jsonArray = new JSONArray();
-        for(int i=0;i<list.size();i++)jsonArray.put(list.get(i));
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                break;
 
-        SharedPreferences sharedPreferences = getSharedPreferences(SCAN_DATA,MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(KEY_MASTAR,jsonArray.toString());
-    }
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                break;
 
-    private ArrayList<String> LoadList(){
-        ArrayList<String> list = new ArrayList<String>();
-        SharedPreferences sharedPreferences = getSharedPreferences(SCAN_DATA,MODE_PRIVATE);
-        String strJson = sharedPreferences.getString(KEY_MASTAR,"");
-        if(!strJson.equals("")){
-            try {
-                JSONArray jsonArray = new JSONArray(strJson);
-                for (int i=0;i<jsonArray.length();i++)list.add(jsonArray.getString(i));
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+            case KeyEvent.KEYCODE_ENTER:
+                Toast.makeText(getApplicationContext(), "Clear price data.", Toast.LENGTH_SHORT).show();
+                SharedPreferences sharedPreferences = getSharedPreferences(SCAN_DATA, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt(KEY_TOTAL, 0);
+                editor.apply();
+                break;
+
+            case KeyEvent.KEYCODE_BACK:
+                finish();
+                break;
+
+            case KeyEvent.KEYCODE_MENU:
+                break;
         }
-        return  list;
+
+        return super.onKeyDown(keyCode, event);
     }
 }
